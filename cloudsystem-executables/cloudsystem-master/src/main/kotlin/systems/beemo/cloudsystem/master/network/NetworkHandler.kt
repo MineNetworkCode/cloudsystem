ce0f128f712523b069ec16016a45fc44501d0169
Jeerrryyyy <j.braun@findologic.com>
2021-11-08 03:46:25 +0100

    Add connection and worker removeing on disconnect

--- cloudsystem-executables/cloudsystem-master/src/main/kotlin/systems/beemo/cloudsystem/master/network/NetworkHandler.kt
+++ cloudsystem-executables/cloudsystem-master/src/main/kotlin/systems/beemo/cloudsystem/master/network/NetworkHandler.kt
@@ -2,20 +2,34 @@ package systems.beemo.cloudsystem.master.network
 
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
+import org.kodein.di.instance
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import systems.beemo.cloudsystem.library.network.protocol.Packet
+import systems.beemo.cloudsystem.master.CloudSystemMaster
+import systems.beemo.cloudsystem.master.worker.WorkerRegistry
 
 class NetworkHandler : SimpleChannelInboundHandler<Packet>() {
 
     private val logger: Logger = LoggerFactory.getLogger(NetworkHandler::class.java)
+
+    private val workerRegistry: WorkerRegistry by CloudSystemMaster.KODEIN.instance()
 
     override fun channelRead0(channelHandlerContext: ChannelHandlerContext, packet: Packet) {
         packet.handle(channelHandlerContext)
     }
 
     override fun channelInactive(channelHandlerContext: ChannelHandlerContext) {
-        logger.info("Some worker disconnected") // TODO: Better message and handle disconnecting worker
+        val workerInfo = workerRegistry.getWorkerByChannel(channelHandlerContext.channel()) ?: return
+        val workerName = "${workerInfo.name}${workerInfo.delimiter}${workerInfo.suffix}"
+
+        workerRegistry.unregisterWorker(workerInfo.uuid)
+
+        logger.info(
+            "Removed connection and unregistered Worker:(Name=$workerName, Remote=${
+                channelHandlerContext.channel().remoteAddress().toString().replace("/", "")
+            })"
+        )
     }
 
     @Throws(Exception::class)
